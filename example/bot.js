9da653369b2852dcad7783c8cc37199be6628fd5
azkadev <insaneazka@gmal.com>
2022-02-07 19:11:42 +0700

    Update more method and support telegram keyboard style

--- example/bot.js
+++ example/bot.js
@@ -9,11 +9,71 @@ wa.on("update", async function (update, update_origin) {
             var is_outgoing = msg["is_outgoing"];
             if (text) {
                 if (RegExp("^/start$", "i").exec(text)) {
+                    var data = {
+                        "chat_id": chat_id,
+                        "text": `Hay perkenalkan saya adalah bot`,
+                        "reply_markup": {
+                            "inline_keyboard": [
+                                [
+                                    {
+                                        "text": "GITHUB",
+                                        "url": "https://github.com/azkadev"
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": "callback",
+                                        "callback_data": "https://github.com/azkadev"
+                                    },
+                                    {
+                                        "text": "callback",
+                                        "callback_data": "https://github.com/azkadev"
+                                    }
+                                ]
+                            ]
+                        }
+                    };
+                    return await wa.request("sendMessage", data);
+                }
+                
+                if (RegExp("^/photo$", "i").exec(text)) {
+                    var data = {
+                        "chat_id": chat_id,
+                        "photo": `https://user-images.githubusercontent.com/38845275/128774296-40a55843-1893-44e6-936e-5e71c7cf72de.png`,
+                        "caption": "ini pesan photo"
+                    };
+                    return await wa.request("sendPhoto", data);
+                }
+
+                if (RegExp("^/jsondumpraw$", "i").exec(text)) {
                     return await wa.request("sendMessage", {
                         "chat_id": chat_id,
-                        "text": "Hay saya adalah robot"
+                        "text": JSON.stringify(update_origin, null, 2)
                     });
                 }
+
+                if (RegExp("^/jsondump$", "i").exec(text)) {
+                    return await wa.request("sendMessage", {
+                        "chat_id": chat_id,
+                        "text": JSON.stringify(update, null, 2)
+                    });
+                }
+
+                if (RegExp("^/ping$", "i").exec(text)) {
+                    var time = (Date.now() / 1000) - msg.date;
+                    var data = {
+                        "chat_id": chat_id,
+                        "text": `Pong ${time.toFixed(3)}`
+                    };
+                    return await wa.request("sendMessage", data);
+                }
+            }
+            if (!is_outgoing) {
+                var data = {
+                    "chat_id": chat_id,
+                    "text": `Hay perkenalkan saya adalah bot\nTolong gunakan perintah /start ya!`,
+                };
+                return await wa.request("sendMessage", data);
             }
         }
     }
--- src/js/index.js
+++ src/js/index.js
@@ -43,38 +43,214 @@ class Whatsapp {
 
     }
 
+    reply_markup(data_keyboard) {
+        try {
+            var keyboards = [];
+            if (typeof data_keyboard == "object") {
+                if (typeof data_keyboard["inline_keyboard"] == "object") {
+                    var inline_keyboard = data_keyboard["inline_keyboard"];
+                    for (var i = 0; i < inline_keyboard.length; i++) {
+                        var loop_data = inline_keyboard[i];
+                        if (typeof loop_data == "object") {
+                            var keyboard_index = (i == 0) ? 1 : (i + 1);
+                            if (loop_data.length == 1) {
+                                var json_data = {
+                                    "index": keyboard_index
+                                };
 
+                                if (typeof loop_data[0]["text"] != "string") {
+                                    throw {
+                                        "status_code": 400,
+                                        "status_bool": false,
+                                        "message": "parameters text keyboard harus string"
+                                    };
+                                }
+                                if (!loop_data[0]["text"]) {
+                                    throw {
+                                        "status_code": 400,
+                                        "status_bool": false,
+                                        "message": "tolong isi textnya juga dong"
+                                    };
+                                }
+                                var json_data_keyboard = {};
+                                json_data_keyboard["displayText"] = String(loop_data[0]["text"]);
+                                if (loop_data[0]["url"]) {
+                                    json_data_keyboard["url"] = String(loop_data[0]["url"]);
+                                    json_data["urlButton"] = json_data_keyboard;
+                                } else if (loop_data[0]["callback_data"]) {
+                                    json_data_keyboard["id"] = 'id-like-buttons-message';
+                                    json_data["quickReplyButton"] = json_data_keyboard;
+                                } else if (loop_data[0]["phone_number"]) {
+                                    json_data_keyboard["phoneNumber"] = String(loop_data[0]["phone_number"]);
+                                    json_data["callButton"] = json_data_keyboard;
+                                }
+                                keyboards.push(json_data);
+                            } else if (loop_data.length > 1) {
+                                for (var ii = 0; ii < loop_data.length; ii++) {
+                                    var loop_dataa = loop_data[ii];
+                                    if (typeof loop_dataa == "object") {
+                                        var json_data = {
+                                            "index": keyboard_index
+                                        };
+
+                                        if (typeof loop_dataa["text"] != "string") {
+                                            throw {
+                                                "status_code": 400,
+                                                "status_bool": false,
+                                                "message": "parameters text keyboard harus string"
+                                            };
+                                        }
+                                        if (!loop_dataa["text"]) {
+                                            throw {
+                                                "status_code": 400,
+                                                "status_bool": false,
+                                                "message": "tolong isi textnya juga dong"
+                                            };
+                                        }
+                                        var json_data_keyboard = {};
+                                        json_data_keyboard["displayText"] = String(loop_dataa["text"]);
+                                        if (loop_dataa["url"]) {
+                                            json_data_keyboard["url"] = String(loop_dataa["url"]);
+                                            json_data["urlButton"] = json_data_keyboard;
+                                        } else if (loop_dataa["callback_data"]) {
+                                            json_data_keyboard["id"] = 'id-like-buttons-message';
+                                            json_data["quickReplyButton"] = json_data_keyboard;
+                                        } else if (loop_dataa["phone_number"]) {
+                                            json_data_keyboard["phoneNumber"] = String(loop_dataa["phone_number"]);
+                                            json_data["callButton"] = json_data_keyboard;
+                                        }
+                                        keyboards.push(json_data);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return keyboards;
+                }
+                return undefined;
+            } else {
+                return undefined;
+            }
+        } catch (e) {
+            return undefined;
+        }
+    }
 
     async request(method, parameters = {}) {
         if (typeof method == "string") {
             if (typeof parameters != "object") {
                 parameters = {};
             }
-            if (RegExp("^sendMessage$", "i").exec(method)) {
+            if (RegExp("^(sendMessage|sendPhoto|sendVideo|sendAudio|sendContact|sendLocation)$", "i").exec(method)) {
                 if (typeof parameters["chat_id"] != "string") {
                     throw {
-                        "message": "params is bad"
+                        "message": "parameters chat_id harus string!"
                     };
                 }
                 if (!parameters["chat_id"]) {
                     throw {
-                        "message": "params is bad"
+                        "message": "parameters chat_id harus isi!"
                     };
                 }
-                if (typeof parameters["text"] != "string") {
-                    throw {
-                        "message": "params is bad"
-                    };
+                if (typeof parameters["reply_markup"] == "object") {
+                    parameters["templateButtons"] = this.reply_markup(parameters["reply_markup"]);
+                    delete parameters["reply_markup"];
                 }
-                if (!parameters["text"]) {
-                    throw {
-                        "message": "params is bad"
-                    };
+                if (RegExp("^sendMessage$", "i").exec(method)) {
+                    if (typeof parameters["text"] != "string") {
+                        throw {
+                            "message": "params is bad"
+                        };
+                    }
+                    if (!parameters["text"]) {
+                        throw {
+                            "message": "params is bad"
+                        };
+                    }
+                    return await this.wa.sendMessage(parameters["chat_id"], {
+                        "text": parameters["text"],
+                        ...parameters
+                    });
                 }
-                return await this.wa.sendMessage(parameters["chat_id"], {
-                    "text": parameters["text"],
-                    ...parameters
-                });
+
+                if (RegExp("^sendPhoto$", "i").exec(method)) {
+                    if (typeof parameters["photo"] != "string") {
+                        throw {
+                            "message": "Tolong parameters photo harus string"
+                        };
+                    }
+                    if (!parameters["photo"]) {
+                        throw {
+                            "message": "parameters photo harus isi"
+                        };
+                    }
+                    parameters["image"] = {
+                        "url": parameters["photo"]
+                    };
+                    delete parameters["photo"];
+                    return await this.wa.sendMessage(parameters["chat_id"], {
+                        ...parameters
+                    });
+                }
+
+                if (RegExp("^sendVideo$", "i").exec(method)) {
+                    if (typeof parameters["video"] != "string") {
+                        throw {
+                            "message": "params is bad"
+                        };
+                    }
+                    if (!parameters["video"]) {
+                        throw {
+                            "message": "params is bad"
+                        };
+                    }
+                    parameters["video"] = parameters["video"];
+                    delete parameters["video"];
+                    return await this.wa.sendMessage(parameters["chat_id"], {
+                        ...parameters
+                    });
+                }
+
+                if (RegExp("^sendAudio$", "i").exec(method)) {
+                    if (typeof parameters["audio"] != "string") {
+                        throw {
+                            "message": "params is bad"
+                        };
+                    }
+                    if (!parameters["audio"]) {
+                        throw {
+                            "message": "params is bad"
+                        };
+                    }
+                    parameters["audio"] = {
+                        "url": parameters["audio"],
+                        "mimetype": "audio/mp3"
+                    };
+                    delete parameters["audio"];
+                    return await this.wa.sendMessage(parameters["chat_id"], {
+                        ...parameters
+                    });
+                }
+
+                if (RegExp("^sendContact$", "i").exec(method)) {
+                    parameters["contact"] = {
+                        "displayName": parameters["name"],
+                        "contacts": parameters["contacts"] 
+                    };
+                    return await this.wa.sendMessage(parameters["chat_id"], {
+                        ...parameters
+                    });
+                }
+                if (RegExp("^sendLocation$", "i").exec(method)) {
+                    parameters["location"] = {
+                        "degreesLatitude": parameters["latitude"],
+                        "degreesLongitude": parameters["longitude"] 
+                    };
+                    return await this.wa.sendMessage(parameters["chat_id"], {
+                        ...parameters
+                    });
+                }
+
             }
         } else {
             throw {
@@ -102,7 +278,7 @@ class Whatsapp {
                         if (typeof message["imageMessage"] == "object" && message["imageMessage"]) {
                             json_data["image"] = message["imageMessage"];
                         }
-                        if (typeof message["locationMessage"] == "object" &&  message["locationMessage"]) {
+                        if (typeof message["locationMessage"] == "object" && message["locationMessage"]) {
                             json_data["location"] = message["locationMessage"];
                         }
                         if (typeof message["contactMessage"] == "object" && message["contactMessage"]) {
